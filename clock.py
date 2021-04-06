7bee912174e310038dbb6419e3c88986c3885651
Daniel Wardwell <62370000+dwardwell1@users.noreply.github.com>
2021-04-06 02:12:57 -0700

    heroku tests

--- clock.py
+++ clock.py
@@ -4,14 +4,19 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=9, minute=15)
 def timed_job():
     new_odds()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=9, minute=16)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    add_avg_spread()
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=9, minute=18)
+def scheduled_job2():
+    avg_book_place()
 
 
 sched.start()
