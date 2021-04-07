e5b8d03fe0c110e928ba3724e5051b934780f231
Daniel Wardwell <62370000+dwardwell1@users.noreply.github.com>
2021-04-07 02:49:39 -0700

    more tests

--- clock.py
+++ clock.py
@@ -1,12 +1,13 @@
 
 from funs import iterate_teams, new_odds, best_val, low_val, add_avg_spread, avg_book_place, show_ranking, rank_teams
 from apscheduler.schedulers.blocking import BlockingScheduler
-from flask import current_app as app
+from flask import current_app
+from app import app
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=9, minute=43)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=9, minute=51)
 def timed_job():
     with app.app_context():
         new_odds()
